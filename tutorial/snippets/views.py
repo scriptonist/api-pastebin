b0d9d0c0a182af8d7f139a903584a5d8c247b469
scriptonist <aravind.techbridge@gmail.com>
2017-03-27 23:27:14 +0530

    Used generic class based views to avoid mixins and make code even more smaller

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -7,32 +7,19 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets or create a new snippet_list
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-
-class SnippetDetail(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, Update or delete a code snippet_detail
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+   
